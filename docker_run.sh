7d79404d296c7c81587863e431a523b6c2611e54
Joshua Higgins <joshuafhiggins@gmail.com>
2025-11-19 16:21:15 -0500

    fix: wrong naming in docker

--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,6 @@ docker run -d \
   --env-file ./.env \
   --name=connect4-moderator-server \
   --restart unless-stopped \
-  -e ADMIN_AUTH="${ADMIN_AUTH}" \
+  -e ADMIN_PASSWORD="${ADMIN_PASSWORD}" \
   -p 5102:8080 \
   joshuafhiggins/connect4-moderator-server
