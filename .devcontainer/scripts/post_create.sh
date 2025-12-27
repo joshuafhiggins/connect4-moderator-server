e488f6dae9f82f4f6f6f305810cf7973a94d574d
Joshua Higgins <joshuafhiggins@gmail.com>
2025-12-27 14:11:22 -0500

    fix: devcontainers bashrc

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -4,6 +4,7 @@ install_devcontainer_cli() {
 	set -e
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 	nvm install 24
+	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 	echo "🔧 Installing DevContainer CLI..."
 	cd "$(dirname "$0")/../tools/devcontainer-cli"
 	npm ci --omit=dev
